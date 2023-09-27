ea12d3034f0d6c7828ab0c4c3e0bebdffd1e66f9
LTien123 <145563112+LTien123@users.noreply.github.com>
2023-09-27 15:11:47 +0700

    em design vo van phan dashboard
    ai co gop y thi bao em hoac clone ve sua nha

--- project/src/main/java/com/controller/project/Adminsite.java
+++ project/src/main/java/com/controller/project/Adminsite.java
@@ -4,19 +4,27 @@ package com.controller.project;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
 
-public class Adminsite {
+public class Adminsite implements Initializable {
     Stage stage = new Stage();
     @FXML
     Button outBut;
+    @FXML
+    BarChart barChart;
 
     public void regissite() throws IOException {
         FXMLLoader loader = new FXMLLoader();
@@ -30,7 +38,7 @@ public class Adminsite {
     public void logout(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(Objects.requireNonNull(getClass().getResource("login.fxml")));
         Scene scene = new Scene(loader.load());
-        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(scene);
         stage.show();
     }
@@ -40,4 +48,16 @@ public class Adminsite {
     }
 
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        XYChart.Series<Object, Object> series1 = new XYChart.Series<>();
+        series1.setName("VND");
+        series1.getData().add(new XYChart.Data<>("19/09", 11));
+        series1.getData().add(new XYChart.Data<>("20/09", 20));
+        series1.getData().add(new XYChart.Data<>("21/09", 14));
+        series1.getData().add(new XYChart.Data<>("22/09", 12));
+        series1.getData().add(new XYChart.Data<>("23/09", 11));
+
+        barChart.getData().addAll(series1);
+    }
 }
